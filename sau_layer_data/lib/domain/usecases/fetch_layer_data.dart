bdb7f65a89b1b2dd7b2e4621255fa20ea78c8b1a
chaiwat.dev <s6052410014@sau.ac.th>
2025-02-27 00:26:19 +0700

    refactor: refactor use case name.

--- sau_layer_data/lib/domain/usecases/fetch_layer_data.dart
+++ sau_layer_data/lib/domain/usecases/fetch_layer_data.dart
@@ -4,10 +4,10 @@ import 'package:sau_layer_data/domain/entities/nasa_layer_data.dart';
 import 'package:sau_layer_data/domain/repositories/nasa_layer_repository.dart';
 
 
-class FetchLayerData {
+class NasaHistory {
   final NasaLayerRepository repository;
 
-  FetchLayerData(this.repository);
+  NasaHistory(this.repository);
 
   Future<Either<Failure, List<NasaLayerData>>> call() async {
     return await repository.nasaHistory();
